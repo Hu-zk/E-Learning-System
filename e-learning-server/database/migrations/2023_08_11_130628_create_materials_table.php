b57d67e8d0e8e38b0ac915b39937ec41bf10a475
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-13 02:04:17 +0300

    Server <> edited materials table

--- e-learning-server/database/migrations/2023_08_11_130628_create_materials_table.php
+++ e-learning-server/database/migrations/2023_08_11_130628_create_materials_table.php
@@ -12,23 +12,24 @@ return new class extends Migration
         Schema::create('materials', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('course_id');
-            $table->boolean('is_announcement');
+            $table->string('title');
             $table->string('description');
-            $table->string('file_url');
+            $table->string('file_url')->nullable();
+            $table->boolean('is_announcement');
             $table->timestamps();
 
             $table->foreign('course_id')->references('id')->on('courses');
         });
 
-        Schema::create('assignments_quizes', function (Blueprint $table) {
+        Schema::create('assignments_quizzes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('course_id');
-            $table->boolean('is_quiz');
             $table->string('title');
             $table->string('description');
             $table->integer('grade');
             $table->datetime('deadline');
             $table->string('file_url');
+            $table->boolean('is_quiz');
             $table->timestamps();
 
             $table->foreign('course_id')->references('id')->on('courses');
@@ -43,17 +44,14 @@ return new class extends Migration
             $table->timestamps();
 
             $table->foreign('student_id')->references('id')->on('users');
-            $table->foreign('assignment_id')->references('id')->on('assignments_quizes');
+            $table->foreign('assignment_id')->references('id')->on('assignments_quizzes');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('materials');
-        Schema::dropIfExists('assignments_quizes');
+        Schema::dropIfExists('assignments_quizzes');
         Schema::dropIfExists('submissions');
     }
 };
