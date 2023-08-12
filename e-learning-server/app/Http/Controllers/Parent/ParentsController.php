f38ae0180f107b1ac2a5fcb91abc5531ea37146b
MhdTarhini <mohamedtarhini95@gmail.com>
2023-08-12 20:41:06 +0300

    server <> Migrate db

---
+++ e-learning-server/app/Http/Controllers/Parent/ParentsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Parents;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ParentsController extends Controller
+{
+    function getParent(){
+        
+    }
+}
