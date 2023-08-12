24c63e9585a21fcbab5cef3148634c115928de30
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-13 00:45:37 +0300

    server <> created getCourse api

--- e-learning-server/routes/api.php
+++ e-learning-server/routes/api.php
@@ -3,25 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Student\TestController;
+use App\Http\Controllers\Teacher\TeacherController;
 
 
-Route::group(['prefix'=> 'user', 'middleware' => 'auth:api'], function(){
+Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
 
-    Route::group(['prefix' => 'student',  'middleware' => 'auth.student'], function(){
-        Route::get('/test', [TestController::class, 'test']);
+    Route::group(['prefix' => 'student',  'middleware' => 'auth.student'], function () {
     });
 
-    Route::group(['prefix' => 'teacher'], function(){
 
+    Route::group(['prefix' => 'parent'], function () {
     });
 
-    Route::group(['prefix' => 'parent'], function(){
-
-    });
-
-    Route::group(['prefix' => 'admin'], function(){
-
+    Route::group(['prefix' => 'admin'], function () {
     });
 
     Route::get("profile", [AuthController::class, "profile"]);
@@ -30,7 +24,12 @@ Route::group(['prefix'=> 'user', 'middleware' => 'auth:api'], function(){
 });
 
 
-Route::group(['prefix' => 'guest'], function(){
+Route::group(['prefix' => 'guest'], function () {
     Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
     Route::post('/login', [AuthController::class, 'login']);
+});
+
+
+Route::group(['prefix' => 'teacher'], function () {
+    Route::get('{teacherId}/courses', [TeacherController::class, 'getCourses']);
 });
