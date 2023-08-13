785a0dbd96946cad9db6e026754f30ab4dffa208
majed <majedhabli2020@gmail.com>
2023-08-13 23:20:06 +0300

    Server <> Updated functions Course stats & Completed Couses.

--- e-learning-server/app/Http/Controllers/Student/CourseController.php
+++ e-learning-server/app/Http/Controllers/Student/CourseController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Course;
 use App\Models\Enrollement;
+use App\Models\AssignmentQuiz;
 use Auth;
 
 class CourseController extends Controller
@@ -50,7 +51,15 @@ class CourseController extends Controller
     // get back to this one.
     function courseStats(){
         $user = Auth::user();
-        $courses = $user->EnrolledCourses()->with('AttendanceByStudent')->where('student_id', $user->id)->get();
+        $user_id = $user->id;
+        // $courses = $user->EnrolledCourses()->with('AttendanceByStudent')->where('student_id', $user->id)->get();
+        $courses = $user->EnrolledCourses()->with(['AttendanceByStudent' => function ($query) use ($user_id) {
+            $query->where('student_id',  $user_id);
+        }])->with(['assignmentsQuizzes' => function ($query) use ($user_id) {
+            $query->withCount(['submissions' => function ($query) use ($user_id) {
+                $query->where('student_id',  $user_id);
+            }]);
+        }])->get();
         // $courses =$user->EnrolledCourses()->with(`AttendanceByStudent($user->id)`)->get();
         return response()->json([
             'status' => 'success',
@@ -62,13 +71,32 @@ class CourseController extends Controller
     //not complete yet
     function completedCourses(){
         $user = Auth::user();
-        $courses = $user->CompletedCourses()->get();
+        $user_id = $user->id;
+        // $courses = $user->CompletedCourses()->with('assignmentsQuizzes', 'assignmentsQuizzes.submissions')->get();
+        $courses = $user->CompletedCourses()->with(['assignmentsQuizzes' => function ($query) use ($user_id) {
+            $query->with(['submissions' => function ($query) use ($user_id) {
+                $query->where('student_id',  $user_id);
+            }]);
+        }])->get();
+
+        // foreach($courses as $course){
+            // assignmentsQuizzes use the hasmany function to get all assignments of specific user
+            // $course = $course->assignmentsQuizzes()->with('submissions')->get();
+            
+            //user belongsTo function when i want to return a specific item belonging to the user
+            //assignmentQuiz
+            // foreach($course as $course_assignment){
+            //     $course_assignment = $course_assignment->assignmentQuiz()->get();
+            // }
+        // }
         // $courses = $user->CompletedCourses()->where('StudentAssignment')->get();
         // $courses = $user->CompletedCourses()->with('StudentAssignment')->get();
 
         return response()->json([
             'status' => 'success',
-            'data' => $courses
+            'data' => $courses,
+            // 'course_id' => $course,
+            // 'course_assignment' => $course_assignment
         ]);
     }
 
--- e-learning-server/app/Models/Course.php
+++ e-learning-server/app/Models/Course.php
@@ -33,7 +33,7 @@ class Course extends Model
         return $this->hasMany(AssignmentQuiz::class, 'course_id');
     }
     
-    public function StudentAssignment(){
-        return $this->hasMany(assignmentsQuizzes::class, 'submissions', 'student_id', 'course_id');//->where('grade', '=', 80);
-    }
+    // public function StudentAssignment(){
+    //     return $this->hasMany(assignmentsQuizzes::class, 'submissions', 'student_id', 'course_id');//->where('grade', '=', 80);
+    // }
 }
