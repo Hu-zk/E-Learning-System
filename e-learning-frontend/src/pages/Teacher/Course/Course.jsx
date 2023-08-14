62edb2b47a4f8193c4f25aca98d495586df169ee
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-08-14 10:28:52 +0300

    styled the grading page

--- e-learning-frontend/src/pages/Teacher/Course/Course.jsx
+++ e-learning-frontend/src/pages/Teacher/Course/Course.jsx
@@ -3,14 +3,21 @@ import Announcement from "../../../components/Teacher/Announcement/Announcement"
 import Material from "../../../components/Teacher/Material/Material";
 import "./style.css"
 import "react-dropdown/style.css";
+import {GrClose} from "react-icons/gr"
 
 const Course = () => {
 
-    let [title, setTitle] = useState("")
-    let [content, setContent] = useState("")
-    let [date, setDate] = useState("")
-    let [grade, setGrade] = useState('')
-    let [file, setFile] = useState('')
+    let [title,
+        setTitle] = useState("")
+    let [content,
+        setContent] = useState("")
+    let [date,
+        setDate] = useState("")
+    let [grade,
+        setGrade] = useState('')
+    let [file,
+        setFile] = useState('')
+    let [isModalOpened, setIsModalOpened] = useState(false)
 
     let [isAnnouncementOpened,
         setIsAnnouncementOpened] = useState(false)
@@ -35,65 +42,95 @@ const Course = () => {
     };
 
     const handleMenuThree = () => {
-      setOpen(false);
+        setOpen(false);
     };
 
     const handleGradeChange = (e) => {
-        if(!isNaN(e.target.value)) {
+        if (!isNaN(e.target.value)) {
             setGrade(e.target.value)
         }
     }
 
     return (
-      <div className="teacher-course-page">
-        <div className="modal">
-            <div className="box">
-                <div><input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="Title" /></div>
-                <div><textarea value={content} onChange={e => setContent(e.target.value)} placeholder="Content" cols="30" rows="10"></textarea></div>
-                <div className="row">
-                    <div><input value={date} onChange={e => setDate(e.target.value)} type="date" placeholder="Deadline" /></div>
-                    <div><input value={grade} onChange={e => handleGradeChange(e)} type="text" placeholder="Grade" /></div>
+        <div className="teacher-course-page">
+            <div className="modal">
+                <div className="box">
+                    <div className="close"><GrClose size={30} className="icon"/></div>
+                    <div><input
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        type="text"
+                        placeholder="Title"/></div>
+                    <div>
+                        <textarea
+                            value={content}
+                            onChange={e => setContent(e.target.value)}
+                            placeholder="Content"
+                            cols="30"
+                            rows="10"></textarea>
+                    </div>
+                    <div className="row">
+                        <div><input
+                            value={date}
+                            onChange={e => setDate(e.target.value)}
+                            type="date"
+                            placeholder="Deadline"/></div>
+                        <div><input
+                            value={grade}
+                            onChange={e => handleGradeChange(e)}
+                            type="text"
+                            placeholder="Grade"/></div>
+                    </div>
+                    <div>
+                        <button onClick={() => fileRef.current.click()} className="upload-file">Upload File</button>
+                    </div>
+                    <input
+                        onChange={e => setFile(e.target.files[0])}
+                        ref={fileRef}
+                        style={{
+                        'display': "none"
+                    }}
+                        className="fileInput"
+                        type="file"/>
                 </div>
-                <div><button onClick={() => fileRef.current.click()} className="upload-file">Upload File</button></div>
-                <input onChange={e => setFile(e.target.files[0])} ref={fileRef} style={{'display': "none"}} className="fileInput" type="file" />
+            </div>
+            <div className="buttons">
+                <div>
+                    <button onClick={handleOpenAnnouncement}>Announce</button>
+                </div>
+                <div>
+                    <Dropdown
+                        className="dropdown"
+                        open={open}
+                        trigger={< button onClick = {
+                        handleOpen
+                    } > Upload Material < /button>}
+                        menu={[ < button onClick = {
+                            handleMenuOne
+                        } > Lecture < /button>, <button onClick={handleMenuTwo}>Quiz</button >, < button onClick = {
+                            handleMenuThree
+                        } > Assignment < /button>, ]}/>
+                </div>
+            </div>
+            <div className="course-name">Course One</div>
+            <div className="stream">
+                <div className="left-stream">
+                    {isAnnouncementOpened && <Announcement/>}
+                    <div className="content">
+                        <Material/>
+                        <Material/>
+                        <Material/>
+                        <Material/>
+                    </div>
+                </div>
+                <div className="right-stream">
+                    <div className="attendance">
+                        <h4>Attendance</h4>
+                        <button>Manage Attendance</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div className="buttons">
-          <div>
-            <button onClick={handleOpenAnnouncement}>Announce</button>
-          </div>
-          <div>
-            <Dropdown
-                className="dropdown"
-              open={open}
-              trigger={<button onClick={handleOpen}>Upload Material</button>}
-              menu={[
-                <button onClick={handleMenuOne}>Lecture</button>,
-                <button onClick={handleMenuTwo}>Quiz</button>,
-                <button onClick={handleMenuThree}>Assignment</button>,
-              ]}
-            />
-          </div>
-        </div>
-        <div className="course-name">Course One</div>
-        <div className="stream">
-          <div className="left-stream">
-            {isAnnouncementOpened && <Announcement />}
-            <div className="content">
-              <Material />
-              <Material />
-              <Material />
-              <Material />
-            </div>
-          </div>
-          <div className="right-stream">
-            <div className="attendance">
-              <h4>Attendance</h4>
-              <button>Manage Attendance</button>
-            </div>
-          </div>
-        </div>
-      </div>
     );
 }
 
