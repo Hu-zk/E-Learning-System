9be7b699c9ae20d668f2dfbaf0af523530a98dbd
majed <majedhabli2020@gmail.com>
2023-08-15 00:48:16 +0300

    Frontend <> Created accordion component and added styling.

--- e-learning-frontend/src/pages/Parent/Home/Home.jsx
+++ e-learning-frontend/src/pages/Parent/Home/Home.jsx
@@ -1,16 +1,29 @@
 import Sidebar from "../../../components/Sidebar/Sidebar";
 import styles from './style.module.css';
+import { Accordion } from "../../../components/accordion/accordion";
 
 const Home = () => {
-
-
+const items = [
+   {
+      title: "Section 1",
+      content: "This is the content of section 1"
+   },
+   {
+      title: "Section 2",
+      content: "This is the content of section 2"
+   },
+   {
+      title: "Section 3",
+      content: "This is the content of section 3"
+   }
+]
     return (
         <div className={styles.container}>
             <div className={styles.page_body}>
                 <div className={styles.body_left}>
                     <div className={styles.page_header}>Announcements</div>
                     <div className={styles.announcements_container}>
-                            <div>-hello</div>
+                        <Accordion items={items}/>
                     </div>
                 </div>
             </div>
