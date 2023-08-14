e5795c3094fbf45fe4e34dca710345882f7608f2
majed <majedhabli2020@gmail.com>
2023-08-14 21:31:28 +0300

    Frontend <> Created the skeletal structure for the home page.

--- e-learning-frontend/src/pages/Parent/Home/Home.jsx
+++ e-learning-frontend/src/pages/Parent/Home/Home.jsx
@@ -1,34 +1,39 @@
-import "./style.css";
 import Sidebar from "../../../components/Sidebar/Sidebar";
 import { useNavigate } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillChatLeftTextFill } from "react-icons/bs";
 import { MdGrade } from "react-icons/md";
+import styles from './style.module.css';
 
 const Home = () => {
     const navigate = useNavigate();
-  const sidebar = [
-    {
-      svg: <AiFillHome size={32} />,
-      onclick: () => navigate("/student"),
-    },
-    {
-      svg: <BsFillChatLeftTextFill size={32} />,
-      onclick: () => navigate("/student/InProcess"),
-    },
-    {
-      svg: <MdGrade size={32} />,
-      onclick: () => navigate("/student/Completed"),
-    },
-  ];
+    const sidebar = [
+        {
+        svg: <AiFillHome size={32} />,
+        onclick: () => navigate("/student"),
+        },
+        {
+        svg: <BsFillChatLeftTextFill size={32} />,
+        onclick: () => navigate("/student/InProcess"),
+        },
+        {
+        svg: <MdGrade size={32} />,
+        onclick: () => navigate("/student/Completed"),
+        },
+    ];
+
     return (
-        <div>
-            {/* <div>Home</div> */}
-        {/* <div> <Sidebar/> </div> */}
-            <Sidebar sidebarContent={sidebar} />
+        <div className={styles.container}>
+            <div>
+                <Sidebar sidebarContent={sidebar}/>
+            </div>
+            <div className={styles.page_body}>
+                <div className={styles.body_left}>left</div>
+                <div className={styles.body_right}>rigth</div>
+            </div>
         </div>
         
     )
 }
 
-export default Home
+export default Home;
