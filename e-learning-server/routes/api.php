2c1cbcffc4c26d6ab1f69d8eef7ebdb6ad28f1fc
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-08-14 18:39:03 +0300

    structured the routes

--- e-learning-server/routes/api.php
+++ e-learning-server/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminUserController;
-use App\Http\Controllers\AdminCourseController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -46,19 +45,19 @@ Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
     });
 
     Route::group(['prefix' => 'admin'], function(){
-        // Route::post('/create-user', [UserController::class, 'createUser']);
-        // Route::put('/update-user/{userId}', [UserController::class, 'updateUser']);
-        // Route::delete('/delete-user/{userId}', [UserController::class, 'deleteUser']);
-        // Route::get('/get-users', [UserController::class, 'getUsers']);
-        // Route::post("/backup", [UserController::class, 'createBackup']);
+        Route::post('/create-user', [UserController::class, 'createUser']);
+        Route::put('/update-user/{userId}', [UserController::class, 'updateUser']);
+        Route::delete('/delete-user/{userId}', [UserController::class, 'deleteUser']);
+        Route::get('/get-users', [UserController::class, 'getUsers']);
+        Route::post("/backup", [UserController::class, 'createBackup']);
 
-        // Route::post('/create-course', [CourseController::class, 'createCourse']);
-        // Route::put('/update-course/{courseId}', [CourseController::class, 'updateCourse']);
-        // Route::get('/course-report/{courseId}', [CourseController::class, 'courseReport']);
-        // Route::get('/teacher-report/{teacherId}', [CourseController::class, 'teacherReport']);
-        // Route::get('/student-report/{studentId}', [CourseController::class, 'studentReport']);
+        Route::post('/create-course', [CourseController::class, 'createCourse']);
+        Route::put('/update-course/{courseId}', [CourseController::class, 'updateCourse']);
+        Route::get('/course-report/{courseId}', [CourseController::class, 'courseReport']);
+        Route::get('/teacher-report/{teacherId}', [CourseController::class, 'teacherReport']);
+        Route::get('/student-report/{studentId}', [CourseController::class, 'studentReport']);
 
-        // Route::post("/update-appearance", [UserController::class, 'updateAppearance']);
+        Route::post("/update-appearance", [UserController::class, 'updateAppearance']);
     });
 
     Route::group(['prefix' => 'teacher',  'middleware' => 'auth.teacher'], function () {
@@ -93,16 +92,4 @@ Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
 
 
 
-Route::post('/create-user', [AdminUserController::class, 'createUser']);
-        Route::post('/update-user/{userId}', [AdminUserController::class, 'updateUser']);
-        Route::delete('/delete-user/{userId}', [AdminUserController::class, 'deleteUser']);
-        Route::get('/get-users', [AdminUserController::class, 'getUsers']);
-        Route::post("/backup", [AdminUserController::class, 'createBackup']);
 
-        Route::post('/create-course', [AdminCourseController::class, 'createCourse']);
-        Route::post('/update-course/{courseId}', [AdminCourseController::class, 'updateCourse']);
-        Route::get('/course-report/{courseId}', [AdminCourseController::class, 'courseReport']);
-        Route::get('/teacher-report/{teacherId}', [AdminCourseController::class, 'teacherReport']);
-        Route::get('/student-report/{studentId}', [AdminCourseController::class, 'studentReport']);
-
-        Route::post("/update-appearance", [AdminUserController::class, 'updateAppearance']);
