b8cd8c8ae33ff226d9c61d576d8f7bc250d09035
majed <majedhabli2020@gmail.com>
2023-08-15 08:37:32 +0300

    Frontend <> Added function get completed courses.

---
+++ e-learning-frontend/src/components/multiUseComp/multiUseComp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './multiUseComp.module.css';
+
+export const SharedComp = ({title,info}) =>{
+    return(
+        <div className={styles.container}>
+            <div className={styles.header}>{title}</div>
+            <div className={styles.details}>{info}</div>
+        </div>
+    )
+}
--- e-learning-frontend/src/pages/Parent/statistics/statistics.jsx
+++ e-learning-frontend/src/pages/Parent/statistics/statistics.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import styles from './statistics.module.css';
 import { Assignment } from "../../../components/assignment/assignmentComp";
+import { SharedComp } from "../../../components/multiUseComp/multiUseComp";
 
 const Statistics = () => {
+    const [notCompleted,setNotCompleted] = useState(false);
+    const [messagebody, setMessage] = useState('');
+    const completed = "completed";
+
+
+    const getCompleted = async () => {
+        const token = localStorage.getItem('jwtToken');
+        const response = await axios.get('http://127.0.0.1:8000/api/user/parent/get_is_completed', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        const data = response.data;
+        if(data.message == 'success'){
+            if(data.data){
+                console.log("enrolled classes are...", data.data);
+            }
+            else{
+                // if (notCompleted){
+                setMessage("no completed courses yet");
+                setNotCompleted(true);
+                console.log(notCompleted);  
+
+                // }
+            }
+        }else{
+            console.log("no enrolled courses.")
+        }
+    }
+
+    //     const getChild = async () => {
+    //     const token = localStorage.getItem('jwtToken');
+    //     const response = await axios.get('http://127.0.0.1:8000/api/user/parent/get_child', {
+    //         headers: {
+    //             'Authorization': `Bearer ${token}`
+    //         }
+    //     });
+    //     const data = response.data;
+    //     if(data.status == 'success'){
+    //         setChildren(data.data);
+    //         console.log('children', children)
+    //     }else{
+    //         console.log("no children exist")
+    //     }
+    // }
+
+    useEffect(()=>{
+        getCompleted();
+        // getChild();
+    },[])
+
     return(
         <div className={styles.container}>
             <div className={styles.page_body}>
@@ -12,7 +65,9 @@ const Statistics = () => {
                         <div className={styles.left_container}>
                             <Assignment/>
                         </div>
-                        <div className={styles.right_container}></div>
+                        <div className={styles.right_container}>
+                            <SharedComp title={completed} info={messagebody}/>{/* add the info*/}
+                        </div>
                     </div>
                 </div>
             </div>
