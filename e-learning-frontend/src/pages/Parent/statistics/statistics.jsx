ab268ca0847df6f22df2fa90cdcfcc265a3b5f02
majed <majedhabli2020@gmail.com>
2023-08-15 05:57:38 +0300

    Frontend <> Updated styling and skeletal structure.

--- e-learning-frontend/src/pages/Parent/statistics/statistics.jsx
+++ e-learning-frontend/src/pages/Parent/statistics/statistics.jsx
@@ -3,7 +3,17 @@ import styles from './statistics.module.css';
 
 const Statistics = () => {
     return(
-        <div className={styles.container}>hello</div>
+        <div className={styles.container}>
+            <div className={styles.page_body}>
+                <div className={styles.body_left}>
+                    <div className={styles.page_header}>Analytics</div>
+                    <div className={styles.stats_container}>
+                        <div className={styles.left_container}></div>
+                        <div className={styles.right_container}></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
