1a6d667bdf609c14ae743a75e7b0bb961e6eda74
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-16 08:18:29 +0300

    Frontend <> Fixed appearance

--- e-learning-frontend/src/pages/Admin/Settings/index.jsx
+++ e-learning-frontend/src/pages/Admin/Settings/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../../components/Admin/Navbar'
 import Appearance from '../../../components/Admin/Appearance';
 import EmailSection from '../../../components/Admin/EmailSection';
-import "./style.css";
 import Backup from '../../../components/Admin/Backup';
+import { sendRequest } from '../../../core/config/request';
+import { requestMethods } from '../../../core/enums/requestMethods';
+import "./style.css";
 
 function Settings() {
 
@@ -11,12 +13,29 @@ function Settings() {
         "title":'Settings :',
     } 
 
+    const [mode, setMode] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () =>{
+            try {
+                const response = await sendRequest({
+                    route: "/user/shared/get-appearance",
+                    method: requestMethods.GET,
+                });
+                setMode(response)
+            } catch (error) {
+                console.error('failed:', error);
+            }
+        }
+        fetchData();
+    }, []);
+
     return (
         <div>
             <Navbar nav={nav}/>
             <div className='setting-page'>
                 <EmailSection/>
-                <Appearance/>
+                <Appearance mode={mode} setMode={setMode}/>
                 <Backup/>
             </div>
         </div>
